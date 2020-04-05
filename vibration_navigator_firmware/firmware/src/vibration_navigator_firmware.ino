308eb90dc09084c169a65c4f2de1031ec5e6e373
Koki Shinjo <sktometometo@gmail.com>
2020-04-05 13:29:19 +0900

    [vibration_navigator_firmware] add loop to imu task and update LCD print sentences

--- vibration_navigator_firmware/firmware/src/vibration_navigator_firmware.ino
+++ vibration_navigator_firmware/firmware/src/vibration_navigator_firmware.ino
@@ -2,11 +2,18 @@
 #define M5STACK_200Q
 // M5Stack and ESP32 headers
 #include <M5Stack.h>
+#include "BluetoothSerial.h"
 // ROS related headers
 #include <ros.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/String.h>
 #include <std_msgs/UInt16MultiArray.h>
+// USER
+#include "bluetooth_hardware.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 #define BUFSIZE 128
 
@@ -17,10 +24,10 @@ void callbackVibrationCommands( const std_msgs::UInt16MultiArray& );
 
 // parameters
 char BluetoothName[] = "M5Stack Vibration Navigator";
-int duration_loop = 10; // [ms]
+int duration_loop = 5; // [ms]
 
 // ROSresources
-ros::NodeHandle nh;
+ros::NodeHandle_<BluetoothHardware> nh;
 std_msgs::UInt16MultiArray msg_vibration_commands;
 sensor_msgs::Imu msg_imu;
 ros::Subscriber<std_msgs::UInt16MultiArray> subscriber_vibration_commands( "~commands", &callbackVibrationCommands);
@@ -56,6 +63,13 @@ float quaternion_z = 0.0F;
 float quaternion_w = 0.0F;
 
 /**
+ * for debug
+ */
+int counterIMU = 0;
+int counterROS = 0;
+unsigned long startTime = 0;
+
+/**
  * Utility
  */
 float rad2deg( float rad ) { return rad * 180 / M_PI; }
@@ -76,30 +90,36 @@ void callbackVibrationCommands( const std_msgs::UInt16MultiArray& msg )
  */
 void taskIMUUpdater( void* pvParameters )
 {
-    M5.IMU.getGyroData(&imu_gyro_x,&imu_gyro_y,&imu_gyro_z);
-    M5.IMU.getAccelData(&imu_acc_x,&imu_acc_y,&imu_acc_z);
-    M5.IMU.getAhrsData(&imu_pitch,&imu_roll,&imu_yaw);
-    M5.IMU.getTempData(&imu_temp);
+    while ( true ) {
+        M5.IMU.getGyroData(&imu_gyro_x,&imu_gyro_y,&imu_gyro_z);
+        M5.IMU.getAccelData(&imu_acc_x,&imu_acc_y,&imu_acc_z);
+        M5.IMU.getAhrsData(&imu_pitch,&imu_roll,&imu_yaw);
+        M5.IMU.getTempData(&imu_temp);
 
-    // https://www.kazetest.com/vcmemo/quaternion/quaternion.htm
-    quaternion_w = cos( imu_roll / 2 )*cos( imu_pitch / 2 )*cos( imu_yaw / 2 ) + sin( imu_roll / 2 )*sin( imu_pitch / 2 )*sin( imu_yaw / 2 );
-    quaternion_x = sin( imu_roll / 2 )*cos( imu_pitch / 2 )*cos( imu_yaw / 2 ) - cos( imu_roll / 2 )*sin( imu_pitch / 2 )*sin( imu_yaw / 2 );
-    quaternion_y = cos( imu_roll / 2 )*sin( imu_pitch / 2 )*cos( imu_yaw / 2 ) + sin( imu_roll / 2 )*cos( imu_pitch / 2 )*sin( imu_yaw / 2 );
-    quaternion_z = cos( imu_roll / 2 )*cos( imu_pitch / 2 )*sin( imu_yaw / 2 ) - sin( imu_roll / 2 )*sin( imu_pitch / 2 )*cos( imu_yaw / 2 );
+        // https://www.kazetest.com/vcmemo/quaternion/quaternion.htm
+        quaternion_w = cos( imu_roll / 2 )*cos( imu_pitch / 2 )*cos( imu_yaw / 2 ) + sin( imu_roll / 2 )*sin( imu_pitch / 2 )*sin( imu_yaw / 2 );
+        quaternion_x = sin( imu_roll / 2 )*cos( imu_pitch / 2 )*cos( imu_yaw / 2 ) - cos( imu_roll / 2 )*sin( imu_pitch / 2 )*sin( imu_yaw / 2 );
+        quaternion_y = cos( imu_roll / 2 )*sin( imu_pitch / 2 )*cos( imu_yaw / 2 ) + sin( imu_roll / 2 )*cos( imu_pitch / 2 )*sin( imu_yaw / 2 );
+        quaternion_z = cos( imu_roll / 2 )*cos( imu_pitch / 2 )*sin( imu_yaw / 2 ) - sin( imu_roll / 2 )*sin( imu_pitch / 2 )*cos( imu_yaw / 2 );
 
-    xSemaphoreTake( xBinarySemaphore, portMAX_DELAY );
-    msg_imu.header.stamp = nh.now();
-    msg_imu.orientation.x = quaternion_x;
-    msg_imu.orientation.y = quaternion_y;
-    msg_imu.orientation.z = quaternion_z;
-    msg_imu.orientation.w = quaternion_w;
-    msg_imu.angular_velocity.x = deg2rad( imu_gyro_x );
-    msg_imu.angular_velocity.y = deg2rad( imu_gyro_y );
-    msg_imu.angular_velocity.z = deg2rad( imu_gyro_z );
-    msg_imu.linear_acceleration.x = imu_acc_x * g_acc;
-    msg_imu.linear_acceleration.y = imu_acc_y * g_acc;
-    msg_imu.linear_acceleration.z = imu_acc_z * g_acc;
-    xSemaphoreGive( xBinarySemaphore );
+        if ( xSemaphoreTake( xBinarySemaphore, ( TickType_t ) 10 ) == pdTRUE ) {
+            msg_imu.header.stamp = nh.now();
+            msg_imu.orientation.x = quaternion_x;
+            msg_imu.orientation.y = quaternion_y;
+            msg_imu.orientation.z = quaternion_z;
+            msg_imu.orientation.w = quaternion_w;
+            msg_imu.angular_velocity.x = deg2rad( imu_gyro_x );
+            msg_imu.angular_velocity.y = deg2rad( imu_gyro_y );
+            msg_imu.angular_velocity.z = deg2rad( imu_gyro_z );
+            msg_imu.linear_acceleration.x = imu_acc_x * g_acc;
+            msg_imu.linear_acceleration.y = imu_acc_y * g_acc;
+            msg_imu.linear_acceleration.z = imu_acc_z * g_acc;
+            xSemaphoreGive( xBinarySemaphore );
+        }
+
+        // M5.Lcd.printf("imu update\n");
+        ++counterIMU;
+    }
 }
 
 void setup()
@@ -110,7 +130,10 @@ void setup()
   M5.begin();
   M5.Power.begin();
   M5.IMU.Init();
-  Serial.begin(115200);
+
+  /**
+   *
+   */
   for ( int i=0; i<num_port; i++ ) {
     ledcSetup( i, frequency, resolution );
     ledcAttachPin( port2pin[i], i );
@@ -123,12 +146,13 @@ void setup()
   M5.Lcd.setTextColor(BLACK);
   M5.Lcd.setCursor(5, 5);
   M5.Lcd.setTextSize(2);
-  M5.Lcd.printf("The Vibration Navigator");
+  M5.Lcd.printf("The Vibration Navigator\n");
+  M5.Lcd.setTextSize(1);
 
   /**
    * ROS Initialization
    */
-  nh.initNode();
+  nh.initNode( BluetoothName );
   if ( not nh.getParam("~imu_frame_id", (char**)&frame_id) ) {
       strcpy( frame_id, "imu_frame" );
   }
@@ -152,15 +176,28 @@ void setup()
           2,
           &xHandle,
           1 );
+
+  M5.Lcd.printf("Initialized\n");
+  M5.Lcd.printf("Please pair with \"%s\"\n", BluetoothName);
+  M5.Lcd.printf("Main loop started\n");
+
+  startTime = millis();
 }
 
 void loop()
 {
+    if ( counterROS % 100 == 0 ) {
+        M5.Lcd.fillRect(5, 50, 2000, 10, WHITE);
+        M5.Lcd.setCursor(5, 50);
+        M5.Lcd.printf("[%d s] ROS loop: %d, IMU loop: %d\n", (millis() - startTime)/1000, counterROS, counterIMU);
+    }
     if ( xBinarySemaphore != NULL ) {
-        xSemaphoreTake( xBinarySemaphore, portMAX_DELAY );
-        publisher_imu.publish( &msg_imu );
-        xSemaphoreGive( xBinarySemaphore );
+        if ( xSemaphoreTake( xBinarySemaphore, ( TickType_t ) 10 ) == pdTRUE ) {
+            publisher_imu.publish( &msg_imu );
+            xSemaphoreGive( xBinarySemaphore );
+        }
     }
     nh.spinOnce();
     delay(duration_loop);
+    ++counterROS;
 }
