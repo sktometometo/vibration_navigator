2308449ce45fc04048f9dcf25609fdd912c09ae9
Koki Shinjo <sktometometo@gmail.com>
2020-05-27 13:34:49 +0900

    update udev_script

--- vibration_navigator_driver/scripts/udev_script.sh
+++ vibration_navigator_driver/scripts/udev_script.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo "# 
-SUBSYSTEMS==\"usb\", ATTRS{serial}==\"01CDD81A\", NAME=\"vibration_navigator_left\",  SYMLINK+=\"vibration_navigator_left\"
-SUBSYSTEMS==\"usb\", ATTRS{serial}==\"01CDD3B8\", NAME=\"vibration_navigator_right\", SYMLINK+=\"vibration_navigator_right\"" \
+SUBSYSTEMS==\"usb\", ATTRS{serial}==\"01CDD81A\", GROUP=\"dialout\", MODE=\"666\", NAME=\"vibration_navigator_left\",  SYMLINK+=\"vibration_navigator_left\"
+SUBSYSTEMS==\"usb\", ATTRS{serial}==\"01CDD4B8\", GROUP=\"dialout\", MODE=\"666\", NAME=\"vibration_navigator_right\", SYMLINK+=\"vibration_navigator_right\"" \
 > /tmp/15-vibration-navigator.rules
 
 sudo sh -c "cat /tmp/15-vibration-navigator.rules > /etc/udev/rules.d/15-vibration-navigator.rules"
 
 sudo udevadm control --reload-rules
+sudo udevadm trigger
 sudo adduser $USER dialout
